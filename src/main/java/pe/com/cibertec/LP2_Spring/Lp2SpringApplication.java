d0f274b708e95aed3a9b83bf96def7a8086e0ed4
juancin10xd <jcgpalacios1@gmail.com>
2024-10-12 15:17:09 -0500

    Estructura inicial de proyecto

---
+++ src/main/java/pe/com/cibertec/LP2_Spring/Lp2SpringApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.LP2_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/cibertec/LP2_Spring/Lp2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.LP2_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
